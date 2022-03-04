b393eb0d8aaf5a6f625a857c74c41a95a24afa45
zhengshunze <zhengshunze2020@gmail.com>
2022-03-03 19:54:15 -0800

    OK

--- AI_Carrefour/poprecording.swift
+++ AI_Carrefour/poprecording.swift
@@ -12,28 +12,7 @@ import AVFoundation
 
 class poprecording: UIViewController {
     
-    var metronomePlay = ViewController()
-       var stopMetronome: (() -> Void)? // stopMetronome closure
-    @IBAction func stopRecordAudio(_ sender: Any) {
-        
-        if let stopMetronome = stopMetronome {
-                stopMetronome() // calling the closure
-             }
-    
-      
-    }
-   
-    func stopMetronome(){
-        
-        guard audioRecorder != nil else {
-                      return
-                  }
-
-                  audioRecorder!.stop()
-                  print("停止錄音")
-        
-    }
-    
+ 
     @IBOutlet weak var recordingGIF: UIImageView!
     
     @IBOutlet weak var popupView: UIView!
