c31855c313de7deaddb10ba69c48503a83545d3b
zhengshunze <zhengshunze2020@gmail.com>
2022-03-03 18:48:53 -0800

    OK

--- AI_Carrefour/ViewController.swift
+++ AI_Carrefour/ViewController.swift
@@ -6,14 +6,53 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    let session = AVAudioSession.sharedInstance()
+    // 錄音用
+    var audioRecorder: AVAudioRecorder?
+    // 播放用
+    var audioPlayer: AVAudioPlayer?
+    // 設定錄音檔路徑與檔名
+    let tmpURL = URL(fileURLWithPath: NSTemporaryDirectory() + "audio.ima4")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        print(tmpURL.absoluteString)
+        // 設定錄音品質
+        let audioSettings = [
+            AVFormatIDKey: NSNumber(value: kAudioFormatMPEG4AAC)
+        ]
+        
+        do {
+            try session.setCategory(.playAndRecord, mode: .measurement, options: [])
+            try session.setActive(true)
+            
+            audioRecorder = try AVAudioRecorder(url: tmpURL, settings: audioSettings)
+        } catch {
+            print(error)
+        }
     }
 
+    
+    @IBAction func recordAudio(_ sender: Any) {
+        guard audioRecorder != nil else {
+                      return
+                  }
+                  
+                  if audioRecorder!.record() {
+                      print("開始錄音")
+                  }
+    
+    }
+    
+  
 
+    
+      
+    
 }
 
--- AI_Carrefour/poprecording.swift
+++ AI_Carrefour/poprecording.swift
@@ -11,7 +11,31 @@ import AVFoundation
 
 
 class poprecording: UIViewController {
+    
+    var metronomePlay = ViewController()
+       var stopMetronome: (() -> Void)? // stopMetronome closure
+    @IBAction func stopRecordAudio(_ sender: Any) {
+        
+        if let stopMetronome = stopMetronome {
+                stopMetronome() // calling the closure
+             }
+    
+      
+    }
+   
+    func stopMetronome(){
+        
+        guard audioRecorder != nil else {
+                      return
+                  }
 
+                  audioRecorder!.stop()
+                  print("停止錄音")
+        
+    }
+    
+    @IBOutlet weak var recordingGIF: UIImageView!
+    
     @IBOutlet weak var popupView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,8 +43,17 @@ class poprecording: UIViewController {
         popupView.layer.cornerRadius =	 10
         popupView.layer.masksToBounds = true
 
+        // load a GIF 
+       
+        recordingGIF.loadGif(name: "wave4")
         // Do any additional setup after loading the view.
+  
+   
     }
+    
+
+  
+  
     
 		
     @IBAction func closepopup(_ sender: Any) {
